a240a2fb5f273b1b0bf2b74ed01e4f49c3a0562e
deepsweet <kir@soulshine.in>
2016-12-24 22:52:44 +0100

    :wrench: switch to `eslint-config-tough`

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,9 +1,9 @@
 import test from 'tape';
 import execa from 'execa';
 
-test('basic', t => {
-    execa('babel-node', [ './lib/index.js' ]).catch((error) => {
-        t.pass('ok');
-        t.end();
-    });
+test('basic', (t) => {
+  execa('babel-node', [ './lib/index.js' ]).catch(() => {
+    t.pass('ok');
+    t.end();
+  });
 });
